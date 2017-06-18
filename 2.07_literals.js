172254aebdcb5ee1c08340f228fc281eef3c5172
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-06-18 15:21:19 -0400

    add brief definitions of literals

---
+++ 2.07_literals.js
@@ -0,0 +1,7 @@
+Object literals are a convenient notation for specifying new objects. 
+The names of the properties can be specified as names or as strings. 
+The names are treated as literal names, not as variable names, 
+so the names of the properties of the object must be known at compile time. 
+The values of the properties are expressions.
+
+Array literals are a convenient notation for specifying new arrays.
