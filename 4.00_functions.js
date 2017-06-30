f91866333e4cf99519e7444f6eee6171099bd100
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-06-30 13:14:58 -0400

    add basic definition of functions

---
+++ 4.00_functions.js
@@ -0,0 +1,15 @@
+The best thing about JavaScript is its implementation of functions. 
+It got almost everything right. 
+But, as you should expect with JavaScript, it didn’t get everything right.
+
+
+A function encloses a set of statements. 
+Functions are the fundamental modular unit of JavaScript. 
+
+
+They are used for code reuse, information hiding, and composition.
+Functions are used to specify the behavior of objects. 
+
+
+Generally, the craft of programming is the factoring of a set of requirements into a set of functions and data structures.
+
