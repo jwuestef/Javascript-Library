112276dc3752c450e6789d251e75b6c9e1635f13
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-08-07 08:56:47 -0400

    sequence of regular expressions

---
+++ 7.04_regexp_sequence.js
@@ -0,0 +1,5 @@
+A regexp sequence contains one or more regexp factors. 
+
+Each factor can optionally be followed by a quantifier that determines how many times the factor is allowed to appear. 
+
+If there is no quantifier, then the factor will be matched one time.
