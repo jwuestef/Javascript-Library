497354aaedac9f6eae373922abdee46546b6f55d
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-08-12 15:24:56 -0400

    escape class for regular expressions

---
+++ 7.09_regexp_class_escape.js
@@ -0,0 +1,6 @@
+The rules of escapement within a character class are slightly different than those for a regexp factor. 
+
+[\b] is the backspace character. 
+
+Here are the special characters that should be escaped in a character class:
+	- / [ \ ] ^
